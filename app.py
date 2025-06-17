7aef4a40513746ffd3979108fc25fa1de607bfba
iyana jones <iyanajones1127@gmail.com>
2025-06-17 18:54:03 +0000

    added routes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# More lines of code
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return"Hello World\n"
+@app.route('/<name>', methods=['GET'])
+def hello_name(name):
+    return f'Hello, {name}!\n'
+
+if __name__ == '__main__':
+    app.run(debug =True, host='0.0.0.0', port='8066')
+
